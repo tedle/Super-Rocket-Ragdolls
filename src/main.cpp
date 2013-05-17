4aeced4990727beb362e9a0d98db1d25989f0ad8
tedle <tedle@blon.se>
2013-05-17 19:15:39 +0100

    Refactored for 80 char line limit, added more comments, browse dialog now defaults to Grim Dawn steam directory

--- src/main.cpp
+++ src/main.cpp
@@ -3,28 +3,33 @@
 
 #include "resource.h"
 
-size_t PatternSearch(PBYTE haystack, size_t haystack_size, PBYTE pattern, size_t pattern_size) {
+// Does a binary pattern search uses Boyer-Moore-Horspool algorithm
+// Returns position of match as size_t
+size_t PatternSearch(PBYTE haystack, size_t haystack_size,
+                     PBYTE needle, size_t needle_size) {
     // Makes string searching easier
-    size_t pattern_last = pattern_size - 1;
+    size_t needle_last = needle_size - 1;
 
-    // Let us boyer-moore-horspool
+    // Zeroing out jump table, without the 0s
     DWORD jump_table[0xFF+1];
     for (int i = 0; i <= 0xFF; i++) {
-        jump_table[i] = pattern_size;
+        jump_table[i] = needle_size;
     }
 
     // Fill up the jump table w/ char's distance to end of needle
-    for (size_t i = 0; i < pattern_last; i++) {
-        jump_table[pattern[i]] = pattern_last - i;
+    for (size_t i = 0; i < needle_last; i++) {
+        jump_table[needle[i]] = needle_last - i;
     }
 
     // Compares starting from end of pattern & haystack[i]
-    for (size_t i = pattern_last; i < haystack_size; i += jump_table[haystack[i]]) {
+    for (size_t i = needle_last; i < haystack_size;
+         i += jump_table[haystack[i]]) {
         // Nested loop iterates through partial matches
-        for (size_t j = pattern_last; pattern[j] == haystack[i-pattern_last+j]; j--) {
+        for (size_t j = needle_last; needle[j] == haystack[i-needle_last+j];
+             j--) {
             // 0 is the end because we iterate backwards, complete match
             if (j == 0) {
-                return i-pattern_last;
+                return i-needle_last;
             }
         }
     }
@@ -32,8 +37,10 @@ size_t PatternSearch(PBYTE haystack, size_t haystack_size, PBYTE pattern, size_t
     return 0;
 }
 
+// Backs up file_location to file_location.bak, expects file already loaded in
+// memory for optimization reasons
+// Returns true on success, false otherwise
 bool BackupDll(const char* file_location, PBYTE file_data, size_t file_size) {
-    // Backup filename to filename.bak
     char dll_backup[1024] = {0};
     strcat_s(dll_backup, file_location);
     strcat_s(dll_backup, ".bak");
@@ -47,11 +54,11 @@ bool BackupDll(const char* file_location, PBYTE file_data, size_t file_size) {
     return true;
 }
 
+// Creates a browse for file dialog using Win32 API
 void BrowseForDll(HWND hwnd) {
-	OPENFILENAMEA ofn;
+    OPENFILENAMEA ofn = {0};
 	char file[1024];
 
-	ZeroMemory(&ofn, sizeof(ofn));
 	ofn.lStructSize = sizeof(ofn);
 	ofn.hwndOwner = hwnd;
 	ofn.lpstrFile = file;
@@ -61,7 +68,7 @@ void BrowseForDll(HWND hwnd) {
 	ofn.nFilterIndex = 2;
 	ofn.lpstrFileTitle = NULL;
 	ofn.nMaxFileTitle = 0;
-	ofn.lpstrInitialDir = "%USERPROFILE%";
+	ofn.lpstrInitialDir = "%PROGRAMFILES(X86)%\\Steam\\steamapps\\common\\Grim Dawn";
 	ofn.Flags = OFN_PATHMUSTEXIST|OFN_FILEMUSTEXIST;
 
 	GetOpenFileNameA(&ofn);
@@ -71,26 +78,28 @@ void BrowseForDll(HWND hwnd) {
 	return;
 }
 
+// Patches Grim Dawn's Engine.dll to provide rocket ragdoll physics
 void PatchSuperRocketRagdolls(HWND hwnd) {
+    // Grab file location from text box
     char dll_location[1024] = {0};
     GetDlgItemTextA(hwnd, IDC_FILE, dll_location, 1024);
 
-    // This pattern searches for the vector.y = vector.y     * vector.force
-    // and changes it to             vector.y = vector.force * vector.force
+    // This pattern searches for vector.y = vector.y     * vector.force
+    // and changes it to         vector.y = vector.force * vector.force
     // FLD DWORD PTR DS:[EAX+4] ---> FLD DWORD PTR DS:[EAX+10]
-    BYTE y_force_pattern[] = { 0xD9, 0x40, 0x04, 0xD8, 0xC9, 0xD9, 0x5D, 0xEC,
-                               0xD8, 0x48, 0x08, 0x8B, 0x03, 0x8B, 0x90, 0x94 };
+    BYTE y_force_pattern[] = {0xD9, 0x40, 0x04, 0xD8, 0xC9, 0xD9, 0x5D, 0xEC,
+                              0xD8, 0x48, 0x08, 0x8B, 0x03, 0x8B, 0x90, 0x94};
     size_t y_force_pattern_size = 16;
-    BYTE y_force_replace[] = { 0xD9, 0x40, 0x10 };
+    BYTE y_force_replace[] = {0xD9, 0x40, 0x10};
     size_t y_force_replace_size = 3;
 
     // This pattern searches for the physics cap, a 32-bit float = 12.0
     // and changes it to                            32-bit float =  9.0
     // 00 00 40 41 ---> 00 00 10 41
-    BYTE force_cap_pattern[] = { 0x00, 0x00, 0x40, 0x41, 0x6F, 0x12, 0x83, 0x3A,
-                                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2E, 0x40 };
+    BYTE force_cap_pattern[] = {0x00, 0x00, 0x40, 0x41, 0x6F, 0x12, 0x83, 0x3A,
+                                0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x2E, 0x40};
     size_t force_cap_pattern_size = 16;
-    BYTE force_cap_replace[] = { 0x00, 0x00, 0x10, 0x41 };
+    BYTE force_cap_replace[] = {0x00, 0x00, 0x10, 0x41};
     size_t force_cap_replace_size = 4;
 
     FILE* file;
@@ -104,22 +113,34 @@ void PatchSuperRocketRagdolls(HWND hwnd) {
     size_t file_size = ftell(file);
     fseek(file, 0, SEEK_SET);
 
-    // Stick it into memory cus C++ has no good file search
+    // File will be loaded into memory, don't wanna explode user's RAM over
+    // a file that should only be 3MB~ in size
+    if (file_size > 1024 * 1024 * 20) {
+        MessageBox(hwnd, L"File exceeds 20MB in size, aborting", L"Error",
+                   MB_OK);
+        fclose(file);
+        return;
+    }
+
+    // Stick file contents into memory cus stdlib has no good file search
     PBYTE file_data = new BYTE[file_size];
     fread(file_data, sizeof(BYTE), file_size, file);
 
     // Backup the dll
     if (!BackupDll(dll_location, file_data, file_size)) {
-        MessageBox(hwnd, L"Failed to create backup of Engine.dll, aborting", L"Error", MB_OK);
+        MessageBox(hwnd, L"Failed to create backup of Engine.dll, aborting",
+                   L"Error", MB_OK);
         fclose(file);
         delete [] file_data;
         return;
     }
 
-    // Find the file positions of our patterns
-    size_t match = PatternSearch(file_data, file_size, y_force_pattern, y_force_pattern_size);
+    // Find the file position of the first pattern
+    size_t match = PatternSearch(file_data, file_size, y_force_pattern,
+                                 y_force_pattern_size);
     if (!match) {
-        MessageBox(hwnd, L"Could not locate physics height force code", L"Error", MB_OK);
+        MessageBox(hwnd, L"Could not locate physics height force code",
+                   L"Error", MB_OK);
         fclose(file);
         delete [] file_data;
         return;
@@ -129,10 +150,12 @@ void PatchSuperRocketRagdolls(HWND hwnd) {
     // The replacing part of search & replace :)
     fwrite(y_force_replace, sizeof(BYTE), y_force_replace_size, file);
 
-    // Repeat everything (it's ugly but a new struct type w/ dynamically allocated arrays seemed like overkill) 
-    match = PatternSearch(file_data, file_size, force_cap_pattern, force_cap_pattern_size);
+    // Find the file position of the second pattern
+    match = PatternSearch(file_data, file_size, force_cap_pattern,
+                          force_cap_pattern_size);
     if (!match) {
-        MessageBox(hwnd, L"Could not locate physics max force code", L"Error", MB_OK);
+        MessageBox(hwnd, L"Could not locate physics max force code",
+                   L"Error", MB_OK);
         fclose(file);
         delete [] file_data;
         return;
@@ -142,6 +165,7 @@ void PatchSuperRocketRagdolls(HWND hwnd) {
     // Last replace
     fwrite(force_cap_replace, sizeof(BYTE), force_cap_replace_size, file);
 
+    // Yay!
     MessageBox(hwnd, L"Engine.dll patched successfully", L"Success", MB_OK);
 
     // Cleanup
@@ -150,11 +174,14 @@ void PatchSuperRocketRagdolls(HWND hwnd) {
     return;
 }
 
+// Dialog window callbacks
+// Returns TRUE when message was handled
 BOOL CALLBACK DialogProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
     switch (msg) {
         case WM_INITDIALOG: {
             // Set icon
-            HICON icon = LoadIcon(GetModuleHandle(0), MAKEINTRESOURCE(IDR_ICON));
+            HICON icon = LoadIcon(GetModuleHandle(0),
+                                  MAKEINTRESOURCE(IDR_ICON));
             SendMessage(hwnd, WM_SETICON, ICON_SMALL, (LPARAM)icon);
             return TRUE;
         }
@@ -178,6 +205,8 @@ BOOL CALLBACK DialogProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
     return FALSE;
 }
 
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmdline, int cmdshow) {
-    return DialogBox(instance, MAKEINTRESOURCE(ID_DLG), nullptr, (DLGPROC)DialogProc);
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, LPSTR cmdline,
+                   int cmdshow) {
+    return DialogBox(instance, MAKEINTRESOURCE(ID_DLG), nullptr,
+                     (DLGPROC)DialogProc);
 }
